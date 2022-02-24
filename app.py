56571093b68a8c2345fa784236deb5d4a233a17a
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2022-02-24 18:36:48 -0500

    fixed submission

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
+import sys
 from flask import Flask, flash, request, redirect, render_template, url_for
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = "code_files"
+UPLOAD_FOLDER = "./code_files/"
 EXTENSIONS = [".py", "java", "c", "cpp"]
 
 app = Flask(__name__)
@@ -17,15 +18,17 @@ def submit():
         if 'code' not in request.files:
             flash("No file, invalid")
             return redirect(request.url)
-        print("files", request.files)
-        print("request", request.form)
+        print("files", request.files, file=sys.stdout)
+        print("request", request.form, file=sys.stdout)
         file = request.files['code']
         if file is not None and file.filename is not "":
-            print(file)
-            file.save(secure_filename(file.filename))
+            print(file, file=sys.stdout)
             file.save(os.path.join(UPLOAD_FOLDER, secure_filename(file.filename)))
             filename = file.filename
             return render_template("submit.html", filename=file.filename)
         else:
             return "<p>Invalid</p>"
     return "<p>Ran</p>"
+
+if __name__ == "__main__":
+    app.run()
